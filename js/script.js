21151498daa8dadd241ef4954f59a0286b3cdf19
Владимир Куклин <privet1vovik@gmail.com>
2023-02-21 22:37:25 +0300

    секция prompt завершена без адаптива

--- js/script.js
+++ js/script.js
@@ -1,26 +1,19 @@
-function minElArr (arr) {
-  let min = arr[0];
-  let indexMin = 0;
-  for ( let i = 1; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-      indexMin = i;
-    }
+const arr = [10, 3, 25, 15, 78, 35, 72, 90, 70];
+
+function sortArr(arr) {
+  if(arr.length < 1) return arr;
+
+  let index = Math.floor(arr.length / 2);
+
+  let less = [];
+  let more = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === index) continue;
+    if (arr[i] < arr[index]) less.push(arr[i]);
+    else more.push(arr[i]);
   }
-  return indexMin;
+  return [...sortArr(less), arr[index], ...sortArr(more)];
 }
 
-function filterArr (arr) {
-  let newArr = [];
-  const length = arr.length;
-  for (let i = 0; i < length; i++) {
-    // debugger
-    let el = minElArr(arr);
-    newArr.push(arr[el]);
-    arr.splice(el, 1);
-    
-  }
-  return newArr;
-}
-console.log(minElArr([3, 1, 7, 2, 8, 6]))
-console.log(filterArr([3, 1, 7, 5, 4, 4, 9]))
+console.log(sortArr(arr))
